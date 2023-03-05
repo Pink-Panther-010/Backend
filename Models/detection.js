dc64b2b62f42d5342a9f5c080d0fb0e6599a8153
אייל פרידמן <u9227119@bsmch.net>
2023-03-05 19:18:22 +0200

    fix(detection.js): BP changes

--- Models/detection.js
+++ Models/detection.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const detectionSchema = new Schema({
-    id: Schema.Types.ObjectId,
-    detections: [{
-        xLocation: Number,
-        yLocation: Number,
-        time: Date
-    }]
-});
-const Detection = mongoose.model('Detection', detectionSchema);
-module.exports = Detection;
+
+module.exports = mongoose => {
+    const Detection = mongoose.model('Detection', mongoose.Schema({
+        detections: [{
+            xLocation: Number,
+            yLocation: Number,
+            time: Date
+        }]
+    })
+    );
+    return Detection;
+};
