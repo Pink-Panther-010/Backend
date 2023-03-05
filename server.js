6eddac5111250ac2c727ea38cb7c73b596e111af
אביב אלון <u9246168@bsmch.net>
2023-03-05 15:24:51 +0200

    feat(detections.controller.js, detections.routes.js, server.js): Added GET request for detections (by ID and general)

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 var cors = require('cors');
+const detectionRouter = require('./routes/detections.routes')
 app.use(cors());
 
+app.use('/detection', detectionRouter);
 
 app.get('/', (req, res) => {
   console.log('Called')
