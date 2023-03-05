551f3f46ced18a6a6bb145ec959e8279e6c8e66a
hashlama05 <hashlama05@bsmch.net>
2023-03-05 17:36:37 +0200

    Fixed project structure

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,8 @@ const controllerProfile = require('@/Controllers/ProfileController');
 
 const router = new express.Router();
 
-router.get('/profile/:id/:licenseNumber/:dangerLevel', controllerProfile.getProfile);
+router.get('/profile/:id/', controllerProfile.getProfile);
+router.get('/profile/:licenseNumber/', controllerProfile.getProfile);
+router.get('/profile/:dangerLevel', controllerProfile.getProfile);
 
 module.exports = router;
