82743ce571176233372726f3d89a507c4e4af58c
PinkPantherAdm <127027062+PinkPantherAdm@users.noreply.github.com>
2023-03-05 16:54:09 +0200

    Create detection.routes.js

--- routes/detection.routes.js
+++ routes/detection.routes.js
@@ -15,6 +15,6 @@ router.get('/:id?', (req, res) => {
     res.send(detections);
 });
 
-router.post('', detectionController.addDetection);
+router.post('/', detectionController.addDetection);
 
 module.exports = router;
