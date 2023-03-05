e5c64da6bf7a93234727f1a3faf354305c75957d
מיכל כביר <u9212168@bsmch.net>
2023-03-05 15:45:42 +0200

    feat(profils):create new file

---
+++ services/profile.js
@@ -0,0 +1,32 @@
+const db = require("../db/db");
+const { ObjectId } = require('mongodb');
+
+exports.getProfileById= async (id) => {
+    return db
+      .get()
+      .collection("profiles")
+      .findOne({
+        _id: ObjectId(id),
+      });
+  
+  };
+  
+  exports.getProfileByLicensePlate= async (licensePlate) => {
+    return db
+      .get()
+      .collection("profiles")
+      .findOne({
+        "licensePlate": licensePlate
+      })
+      
+  };
+
+  exports.getProfileByDifField= async (fieldName, fieldValue) => {
+    return db
+      .get()
+      .collection("profiles")
+      .findMany({
+        fieldName: fieldValue
+      })
+      
+  };
