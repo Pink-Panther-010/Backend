c2bee2354a416f0a3109668ecde3051809de1b34
דניאל נחמן <u9157635@bsmch.net>
2023-03-05 15:40:24 +0200

    feat(db.config.js) created a config  file

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    url: "mongodb://localhost:27017/bezkoder_db"
+    url: "mongodb://localhost:27017/bezkoder_db",
+    host: '',
+    port: '',
+    username: '',
+    password: ''
   };
