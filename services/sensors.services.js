396f81b1c8bf28038d206d2f5b674337b1a278f0
דניאל נחמן <u9157635@bsmch.net>
2023-03-05 20:04:17 +0200

    feat(sensors.service.js) added a function

--- services/sensors.services.js
+++ services/sensors.services.js
@@ -1,10 +1,14 @@
 const db = require("../models/db");
 const sensors = db.sensors;
 
- const getSensorById = (id) => {
+const getSensorById = (id) => {
     try {
-        return sensors.findOne({id : id});
+        return sensors.findOne({ id: id });
     } catch (err) {
         console.log("There was a problem finding the sensor" + err.massage)
     }
- }
+}
+
+module.exports = {
+    getSensorById
+}
