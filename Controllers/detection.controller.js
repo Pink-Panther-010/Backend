c5ee1a65496bbf82f4ec7428dd13e20115065dcb
דניאל נחמן <u9157635@bsmch.net>
2023-03-05 19:15:42 +0200

    feat(detection.controller.js) added a function

--- Controllers/detection.controller.js
+++ Controllers/detection.controller.js
@@ -1,16 +1,32 @@
+const profileService = ('../services/profile.js');
+const detectionService = ('../services/detections.services.js');
+const sensorsService = ('../services/sensors.services.js');
+
 const getAllDetections = () => {
 
 }
 
 const getDetectionsById = (id) => {
-    
+
 }
 
-const addDetection = () => {
+const addDetection = (req, res) => {
+    try {
+        const profile = profileService.getProfileByLicensePlate(req.body.license_plate);
+        //TODO: Retrieve User's danger level via BI team
+        //TODO: Store suspect in DB if danger level > threshold
+        const sensor = sensorsService.findById(req.body.sensorId);
+        const detection = { locX: sensor.locX, locY: sensor.locY, time: new Date() }
+        detectionService.createDetection(detection, profile.id);
 
+        res.status(201);
+    } catch (err) {
+        res.status(500).send("there was a problem" + err.massage);
+    }
 }
 
 module.exports = {
     getAllDetections,
     getDetectionsById,
-    addDetection}
+    addDetection
+}
