545e2810c1dbbe6ee3ee5dd6fe781ca59bd1b419
רועי שורץ <u9275297@bsmch.net>
2023-03-06 10:11:27 +0200

    added model for suspect

--- models/suspect.models.js
+++ models/suspect.models.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose =>{
 const suspects = mongoose.model('suspects', mongoose.Schema({
-    danger_level: { type: Number, max: 5 },
+    danger_level: { type: Number, max: 3 },
 })
 );
 return suspects;
