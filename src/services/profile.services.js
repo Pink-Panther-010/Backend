100f9cdd59a069bd5a210448c7c78e26054476f2
SamPan96 <samuelpanzieri@yahoo.it>
2023-03-07 18:30:04 +0200

    Trying to fix getByLicensePlate

--- src/services/profile.services.js
+++ src/services/profile.services.js
@@ -18,7 +18,7 @@ catch(err){
 };
 
 exports.getProfileByLicensePlate = async (licensePlate) => {
-  const respond = await Profile.findOne(licensePlate ? { license_plates: [licensePlate] } : {})
+  const respond = await Profile.find(licensePlate ? { license_plates: { $in: [licensePlate]} } : {})
   if( respond === undefined) {
     throw new Error("Profile not found");
   }
