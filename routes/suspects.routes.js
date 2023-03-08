bc2ce97dd20c3921cc740071f14a5a31fe55cec8
hashlama05 <hashlama05@bsmch.net>
2023-03-08 10:16:13 +0200

    add sensors json

---
+++ routes/suspects.routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const controllerSuspect = require('@/Controllers/SuspectController');
+
+const router = new express.Router();
+
+
+/*
+router.get('/suspects/:id?', (req, res) => {
+    const id = req.params.id;
+    let suspect;
+
+    if(!id) {
+        suspect = controllerSuspect.getSuspects();
+    } else{
+        suspect = controllerSuspect.getSuspectsById();
+    }
+
+    res.send(suspect)
+})
+*/
+module.exports = router;
+
+router.get('suspects/:id?',controllerSuspect.getSuspect)
