b664f4f32261444271556e3f94478aa801092dfe
PinkPantherAdm <127027062+PinkPantherAdm@users.noreply.github.com>
2023-03-09 09:53:41 +0200

    Update dbMedori.config.js

--- src/config/dbMedori.config.js
+++ src/config/dbMedori.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    url: 'mongodb://localhost:27107/medori?retryWrites=true&w=majority',
-    user:'karnafim-best',
-    pwd:'asdfghjkasdfghjk'
+    url: 'mongodb://karnafim-best:asdfghjkasdfghjk@localhost:27107/medori?retryWrites=true&w=majority',
   };
